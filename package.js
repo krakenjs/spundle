605152f0a4bdc9ffc2f4e3148977f08011d3d7e4
Aria Stewart <aredridel@dinhe.net>
2014-11-04 16:33:18 -0500

    Separate out command-line concern, add option parser for CLI; allow selection of languages and countries

--- package.js
+++ package.js
@@ -4,36 +4,23 @@ var gloob = require('./gloob');
 var async = require('async');
 var path = require('path');
 var glob = require('glob');
+var iferr = require('iferr');
 
-var nopt = require('nopt');
-
-var conf = nopt({
-    "language": String,
-    "country": String
-}, {
-    "l": "--language",
-    "c": "--country"
-});
-
-var base = path.resolve('locales');
-glob(path.resolve(base, path.join(conf.country || '*', conf.language || '*')), function (err, files) {
-    if (err) {
-        throw err;
-    }
-
-    var out = {};
-    async.eachSeries(files, function (ent, next) {
-        gloob(ent, function (err, o) {
-            out[path.relative(base, ent).replace(/(.*)\/(.*)/, "$2-$1")] = o;
-            next();
-        });
-    }, function (err) {
+module.exports = function (base, country, language, callback) {
+    glob(path.resolve(base, path.join(country, language)), function (err, files) {
         if (err) {
             throw err;
         }
 
-        process.stdout.write(JSON.stringify(out));
+        var out = {};
+        async.eachSeries(files, function (ent, next) {
+            gloob(ent, function (err, o) {
+                out[path.relative(base, ent).replace(/(.*)\/(.*)/, "$2-$1")] = o;
+                next();
+            });
+        }, iferr(callback, function () {
+            callback(err, out);
+        }));
+
     });
-
-});
-
+};
